f9e201dd2771d9fd2cd81ab742b45609b181cad2
Ivan <ivan24891@mail.ru>
2023-03-10 17:48:20 +0300

    fix problems

--- js/main.js
+++ js/main.js
@@ -4,13 +4,9 @@
 function randomValue (a, b) {
   const minValue = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const maxValue = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  let coefficient = Math.random();
-
-  while (coefficient === 0) {
-    coefficient = Math.random();
-  }
+  const coefficient = Math.random();
 
   return Math.floor(coefficient * (maxValue - minValue + 1)) + minValue;
 }
 
-const maxCommntLenght = (lineToCheck, maxLength) => String(lineToCheck).length <= maxLength;
+const maxCommentLength = (lineToCheck, maxLength) => String(lineToCheck).length <= maxLength;
