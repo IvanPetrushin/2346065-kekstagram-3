b4b0cec15479dfd4f23a7370637a295f89a889c5
Ivan <ivan24891@mail.ru>
2023-03-17 17:32:53 +0300

    fix naming

--- js/data.js
+++ js/data.js
@@ -3,14 +3,13 @@ import { getRandomInt } from './util.js';
 function getPhotosData() {
   const photos = [];
   for (let i = 0; i < 25; i ++) {
-    const photoData = {
+    photos.push({
       id: i + 1,
       url: `photos/${i + 1}.jpg`,
       description: `Фотокарточка c id = ${i + 1}`,
       likes: getRandomInt(15, 200),
       comments: getRandomInt(0, 200)
-    };
-    photos.push(photoData);
+    });
   }
   return photos;
 }
