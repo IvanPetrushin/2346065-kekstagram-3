90efc179e625d0e3f444efc662820d54548fc9af
Ivan <ivan24891@mail.ru>
2023-04-21 16:40:30 +0300

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@
 import { getPhotosData } from './data.js';
 import { pictures } from './render.js';
 import './bigPicture.js';
+import './photoEditor.js';
+import './upload.js';
+import './photoEditor.js';
